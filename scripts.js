5fe14490f108e99f0e0187a27e2175bdd6f48d46
Ian <ian.h.grant@gmail.com>
2014-03-09 11:39:51 -0700

    styling updates

--- scripts.js
+++ scripts.js
@@ -2,17 +2,20 @@
 
 window.setInterval(function () {
     'use strict';
-    var c = document.getElementById('colors').style.backgroundColor.toString();
-    c = c.substring(c.indexOf('(') + 1, c.indexOf(')')).split(',');
-    var r = rando(+c[0], -5, 5),
-        g = rando(+c[1], -5, 5),
-        b = rando(+c[2], -2, 2);
     try {
-        document.getElementById('colors-counter').style.color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        var c = document.getElementsByClassName('colors')[0].style.backgroundColor.toString();
+        c = c.substring(c.indexOf('(') + 1, c.indexOf(')')).split(',');
+        var r = rando(+c[0], -5, 5),
+            g = rando(+c[1], -5, 5),
+            b = rando(+c[2], -2, 2),
+            colors = document.getElementsByClassName('colors');
+//        document.getElementById('colors-counter').style.color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        for (var x=0; x<colors.length; x++) {
+            colors[x].style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+        };
     } catch (e) {
+        console.log(e.message);
     }
-    document.getElementById('colors').style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-//    document.getElementById('colors').innerHTML = 'rgb(' + r + ',' + g + ',' + b + ')';
 }, 800);
 
 function rando(v, n, x) {
