51f0f251201a2fe58dd23f88a7b5253eaa881744
Ian <ian.h.grant@gmail.com>
2014-03-09 10:04:14 -0700

    more stuff!

---
+++ ihgrant.github.io/scripts.js
@@ -0,0 +1,28 @@
+/* scripts for blog.chenghiz.net */
+
+window.setInterval(function () {
+    'use strict';
+    var c = document.getElementById('colors').style.backgroundColor.toString();
+    c = c.substring(c.indexOf('(') + 1, c.indexOf(')')).split(',');
+    var r = rando(+c[0], -5, 5),
+        g = rando(+c[1], -5, 5),
+        b = rando(+c[2], -2, 2);
+    try {
+        document.getElementById('colors-counter').style.color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    } catch (e) {
+    }
+    document.getElementById('colors').style.color = 'rgb(' + (r-20) + ',' + (g-20) + ',' + (b-20) + ')';
+//    document.getElementById('colors').innerHTML = 'rgb(' + r + ',' + g + ',' + b + ')';
+}, 800);
+
+function rando(v, n, x) {
+    'use strict';
+	v += Math.floor(Math.random() * (x - n + 1) + n);
+	if (v > 235) {
+		return 235;
+    } else if (v < 0) {
+		return 0;
+    } else {
+		return v;
+    }
+}
