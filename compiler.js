ba79aa0c2bec439c151a8b89cc5bd8d218730913
Ian <ian.h.grant@gmail.com>
2015-08-09 10:49:23 -0700

    work on index page

--- compiler.js
+++ compiler.js
@@ -9,36 +9,12 @@ var engine = new Liquid.Engine;
 
 Promise.promisifyAll(fs);
 
+var makePosts = require('./make-posts');
 var POSTS_DIR = './_posts';
 var LAYOUTS_DIR = './_layouts';
 var SITE_DIR = './_site';
 var SITE_VARS = {
-	url: '/'
-};
-
-/* TODO: populate SITE_VARS while building posts so that we have a complete list
- * of posts that we can use to build a list for archives, and to populate the
- * index.
- */
-
-var readFile = function (dir, filename) {
-	return fs.readFileAsync(path.join(dir, filename), {encoding: 'utf-8'})
-		.then(function (contents) {
-			return {
-				name: path.parse(filename).name,
-				contents: contents
-			};
-		})
-		.catch(console.error);
-};
-
-var rearrange = function (layouts) {
-	// take an array of layouts and return them as a dict by name
-	var newLayouts = {};
-	layouts.forEach(function (layout) {
-		newLayouts[layout.name] = layout.contents;
-	});
-	return newLayouts;
+	url: ''
 };
 
 var renderLayout = function (layout) {
@@ -46,62 +22,41 @@ var renderLayout = function (layout) {
 	return layout.render(this.vars);
 };
 
-var frontMatter = function (post) {
-	post.contents = fm(post.contents);
-	return post;
-};
-
-var formatPost = function (post) {
-	var postVars = post.contents.attributes;
-	var name = parseName(post.name);
-
-	postVars.content = marked(post.contents.body);
-	postVars.filepath = path.join(SITE_DIR, name.year, name.month, name.day);
-
-	return {
-		name: name.title,
-		vars: postVars
-	};
-};
-
-var makeDir = function (file) {
-	this.file = file;
-	return mkdirp(this.vars.filepath);
-};
-
 var saveFile = function (file) {
 	var filepath = path.join(this.vars.filepath, this.name + '.html');
 	return fs.writeFileAsync(filepath, this.file);
 };
 
-var parseName = function (filename) {
-	filename = filename.split('-');
+makePosts(SITE_DIR, LAYOUTS_DIR, POSTS_DIR)
+.map(function (post) {
+	// the actual path to the post when the './_site' directory is actually the root
+	var rootUrl = path.normalize(SITE_DIR);
+	var truePath = post.vars.filepath.replace(rootUrl, '');
+	var fileName = post.name + '.html';
+
 	return {
-		year: filename.shift(),
-		month: filename.shift(),
-		day: filename.shift(),
-		title: filename.join('-')
+		title: post.name,
+		contents: post.vars.contents,
+		url: path.join(truePath, fileName),
 	};
-};
-
-var layouts = fs.readdirAsync(LAYOUTS_DIR)
-	.map(readFile.bind(this, LAYOUTS_DIR))
-	.catch(console.error);
-
-var posts = fs.readdirAsync(POSTS_DIR)
-	.map(readFile.bind(this, POSTS_DIR))
-	.map(frontMatter)
-	.map(formatPost)
-	// .tap(console.log)
-	.catch(console.error);
-
-Promise.join(layouts, posts, function (layouts, posts) {
-	layouts = rearrange(layouts);
-	posts.forEach(function (post) {
-		engine.parse(layouts[post.vars.layout]).bind(post)
-		.then(renderLayout)
-		.then(makeDir)
-		.then(saveFile)
+})
+.then(function (posts) {
+	SITE_VARS.posts = posts;
+	return SITE_VARS;
+})
+.then(function (SITE_VARS) {
+	return fs.readFileAsync('./index.html', {encoding: 'utf-8'})
+		.then(function (file) {
+			return fm(file);
+		})
+		.then(function (file) {
+			return engine.parseAndRender(file.body, {site: SITE_VARS});
+		})
+		// .tap(console.log)
+		.then(function (result) {
+			return fs.writeFileAsync(path.join(SITE_DIR, 'index.html'), result);
+		})
 		.catch(console.error);
-	});
-});
+})
+.then(console.log)
+.catch(console.error);
---
+++ make-posts.js
@@ -0,0 +1,101 @@
+var fs = require('fs');
+var path = require('path');
+var Promise = require('bluebird');
+var fm = require('front-matter');
+var marked = require('marked');
+var mkdirp = Promise.promisifyAll(require('mkdirp'));
+var Liquid = require("liquid-node");
+var engine = new Liquid.Engine;
+
+Promise.promisifyAll(fs);
+
+var readFile = function (dir, filename) {
+	return fs.readFileAsync(path.join(dir, filename), {encoding: 'utf-8'})
+		.then(function (contents) {
+			return {
+				name: path.parse(filename).name,
+				contents: contents
+			};
+		})
+		.catch(console.error);
+};
+
+var rearrange = function (layouts) {
+	// take an array of layouts and return them as a dict by name
+	var newLayouts = {};
+	layouts.forEach(function (layout) {
+		newLayouts[layout.name] = layout.contents;
+	});
+	return newLayouts;
+};
+
+var renderLayout = function (layout) {
+	// take a post and a template and render the post using the template
+	return layout.render(this.vars);
+};
+
+var frontMatter = function (post) {
+	post.contents = fm(post.contents);
+	return post;
+};
+
+var makeDir = function (file) {
+	this.file = file;
+	return mkdirp(this.vars.filepath);
+};
+
+var saveFile = function (file) {
+	var filepath = path.join(this.vars.filepath, this.name + '.html');
+	return fs.writeFileAsync(filepath, this.file);
+};
+
+var parseName = function (filename) {
+	filename = filename.split('-');
+	return {
+		year: filename.shift(),
+		month: filename.shift(),
+		day: filename.shift(),
+		title: filename.join('-')
+	};
+};
+
+var makePosts = function (SITE_DIR, LAYOUTS_DIR, POSTS_DIR) {
+	var formatPost = function (post) {
+		var postVars = post.contents.attributes;
+		var name = parseName(post.name);
+
+		postVars.contents = marked(post.contents.body);
+		postVars.filepath = path.join(SITE_DIR, name.year, name.month, name.day);
+
+		return {
+			name: name.title,
+			vars: postVars
+		};
+	};
+
+	var layouts = fs.readdirAsync(LAYOUTS_DIR)
+		.map(readFile.bind(this, LAYOUTS_DIR))
+		.catch(console.error);
+
+	var posts = fs.readdirAsync(POSTS_DIR)
+		.map(readFile.bind(this, POSTS_DIR))
+		.map(frontMatter)
+		.map(formatPost)
+		// .tap(console.log)
+		.catch(console.error);
+
+	return Promise.join(layouts, posts, function (layouts, posts) {
+		layouts = rearrange(layouts);
+		return posts.map(function (post) {
+			return engine.parse(layouts[post.vars.layout]).bind(post)
+				.then(renderLayout)
+				.then(makeDir)
+				.then(saveFile)
+				.then(function () {
+					return this;
+				});
+		});
+	});
+};
+
+module.exports = makePosts;
